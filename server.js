cdb818c382e6d58be1dc0554fd0974415fc6def1
Nirel Ibarra <62606372+knee-rel@users.noreply.github.com>
2024-02-15 18:18:31 +0800

    user auth endpoint

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
